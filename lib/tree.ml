523a538778f24c91b9f2219ac6c7390419ee5b20
Tarazonaa <andres.tara.so@gmail.com>
2026-03-03 20:36:41 -0600

    feat: leftist heaps

--- lib/tree.ml
+++ lib/tree.ml
@@ -1,8 +1,47 @@
 type 'a tree = Empty | Node of 'a tree * 'a * 'a tree
-let rec insert node = function
-    | Empty -> Node (Empty, node, Empty)
-    | Node (left, value, right) -> 
-        if node > value then Node (left, value, insert node right)
-        else if node < value then Node (insert node left, value, right)
-        else Node (left, value, right)
 
+let rec insert node = function
+  | Empty ->
+      Node (Empty, node, Empty)
+  | Node (left, value, right) ->
+      if node > value then Node (left, value, insert node right)
+      else if node < value then Node (insert node left, value, right)
+      else Node (left, value, right)
+
+type 'a leftist = Empty | Node of int * 'a * 'a leftist * 'a leftist
+
+let rank = function Empty -> 0 | Node (r, _, _, _) -> r
+
+let makeT (x, a, b) =
+  if rank a >= rank b then Node (rank b + 1, x, a, b)
+  else Node (rank a + 1, x, b, a)
+
+let empty () = Empty
+
+let isEmpty = function Empty -> true | Node _ -> false
+
+let rec merge leftist1 leftist2 =
+  match leftist1 with
+  | Empty ->
+      leftist2
+  | Node (_, x, a1, b1) -> (
+    match leftist2 with
+    | Empty ->
+        leftist1
+    | Node (_, y, a2, b2) ->
+        if x <= y then makeT (x, a1, merge b1 leftist2)
+        else makeT (y, a2, merge leftist2 b1) )
+
+let insert x h = merge (Node (1, x, Empty, Empty)) h
+
+let findMin = function
+  | Empty ->
+      failwith "Cannot find minimum with empty tree"
+  | Node (_, x, _, _) ->
+      x
+
+let deleteMin = function
+  | Empty ->
+      failwith "Cannot delete minimum from empty tree."
+  | Node (_, x, a, b) ->
+      merge a b
