eab9ed64913006754a5d1f81923fd0cc24dae111
Tarazonaa <andres.tara.so@gmail.com>
2026-03-03 22:38:48 -0600

    feat: make them all functors...

--- lib/stack.ml
+++ lib/stack.ml
@@ -1,11 +1,20 @@
-type 'a t = 'a list
-let empty = []
-let isEmpty s = match s with
-    [] -> true | s -> false
+module Stack (Ord : Set.OrderedType) = struct
+  type t = Ord.t list
 
-let cons a b = a :: b
-let head s = List.nth s 0
-let tail s = List.nth s (List.length s - 1)
-let pop s = match s with
-    [] -> failwith "Cannot pop from empty stack"
-    | h::t -> (h, t)
+  let empty = []
+
+  let isEmpty s = match s with [] -> true | s -> false
+
+  let cons a b = a :: b
+
+  let head s = List.nth s 0
+
+  let tail s = List.nth s (List.length s - 1)
+
+  let pop s =
+    match s with
+    | [] ->
+        failwith "Cannot pop from empty stack"
+    | h :: t ->
+        (h, t)
+end
--- lib/tree.ml
+++ lib/tree.ml
@@ -8,40 +8,42 @@ let rec insert node = function
       else if node < value then Node (insert node left, value, right)
       else Node (left, value, right)
 
-type 'a leftist = Empty | Node of int * 'a * 'a leftist * 'a leftist
+module Leftist (Ord : Set.OrderedType) = struct
+  type leftist = Empty | Node of int * Ord.t * leftist * leftist
 
-let rank = function Empty -> 0 | Node (r, _, _, _) -> r
+  let rank = function Empty -> 0 | Node (r, _, _, _) -> r
 
-let makeT (x, a, b) =
-  if rank a >= rank b then Node (rank b + 1, x, a, b)
-  else Node (rank a + 1, x, b, a)
+  let makeT (x, a, b) =
+    if rank a >= rank b then Node (rank b + 1, x, a, b)
+    else Node (rank a + 1, x, b, a)
 
-let empty () = Empty
+  let empty () = Empty
 
-let isEmpty = function Empty -> true | Node _ -> false
+  let isEmpty = function Empty -> true | Node _ -> false
 
-let rec merge leftist1 leftist2 =
-  match leftist1 with
-  | Empty ->
-      leftist2
-  | Node (_, x, a1, b1) -> (
-    match leftist2 with
+  let rec merge leftist1 leftist2 =
+    match leftist1 with
     | Empty ->
-        leftist1
-    | Node (_, y, a2, b2) ->
-        if x <= y then makeT (x, a1, merge b1 leftist2)
-        else makeT (y, a2, merge leftist2 b1) )
+        leftist2
+    | Node (_, x, a1, b1) -> (
+      match leftist2 with
+      | Empty ->
+          leftist1
+      | Node (_, y, a2, b2) ->
+          if Ord.compare x y <= 0 then makeT (x, a1, merge b1 leftist2)
+          else makeT (y, a2, merge leftist2 b1) )
 
-let insert x h = merge (Node (1, x, Empty, Empty)) h
+  let insert x h = merge (Node (1, x, Empty, Empty)) h
 
-let findMin = function
-  | Empty ->
-      failwith "Cannot find minimum with empty tree"
-  | Node (_, x, _, _) ->
-      x
+  let findMin = function
+    | Empty ->
+        failwith "Cannot find minimum with empty tree"
+    | Node (_, x, _, _) ->
+        x
 
-let deleteMin = function
-  | Empty ->
-      failwith "Cannot delete minimum from empty tree."
-  | Node (_, x, a, b) ->
-      merge a b
+  let deleteMin = function
+    | Empty ->
+        failwith "Cannot delete minimum from empty tree."
+    | Node (_, x, a, b) ->
+        merge a b
+end
